4c464af0cf9a34bdbe2c60a2d3ed21a9d21a516d
fernandes.pinheiro2002 <fernandes.pinheiro2002@gmail.com>
2024-09-19 01:41:47 +0000

    att

--- src/main/java/Zeus/API/ZEUS/Dto/DadosCadastroLogin.java
+++ src/main/java/Zeus/API/ZEUS/Dto/DadosCadastroLogin.java
@@ -1,3 +1,14 @@
+package Zeus.API.ZEUS.Dto;
+
+import Zeus.API.ZEUS.Model.User;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+
+public record DadosCadastroLogin(
+        @NotBlank
+        String login,
+        @NotBlank
+        String senha
 
 
 ) {
